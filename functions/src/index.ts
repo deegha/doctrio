ccc1c3bd9a274fec83cc59ee8f1012e869d3ad8e
Different <13asanka13@gmail.com>
2021-04-15 16:57:46 +0530

    initial commit

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as functions from 'firebase-functions';
-import { feedController } from "./controllers/feed-controller"
-
-
-// export const createAlgolia = functions.firestore.document('feeds/{feedId}').onCreate(feedController.createItemOnAlgolia)
-
-
